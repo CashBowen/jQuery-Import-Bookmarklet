fe9c23c3ab054417e286a69a1523e510f7ab56f2
-Corgi- <99050972+Cor-gi@users.noreply.github.com>
2022-03-09 15:54:57 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
+window.jQuery_import = function() {
 var importjquery = document.createElement('script');
 var jqueryv = '3.6.0';
 importjquery.src = "//ajax.googleapis.com/ajax/libs/jquery/"+jqueryv+"/jquery.min.js";
 document.head.appendChild(importjquery);
+}
