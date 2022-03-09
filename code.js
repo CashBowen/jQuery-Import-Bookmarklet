c838f0cf9e466457f9db7207c1b1a841726c62bb
-Corgi- <99050972+Cor-gi@users.noreply.github.com>
2022-03-09 15:18:56 -0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,4 @@
+var importjquery = document.createElement('script');
+var jqueryv = '3.6.0';
+importjquery.src = "//ajax.googleapis.com/ajax/libs/jquery/"+jqueryv+"/jquery.min.js";
+document.head.appendChild(importjquery);
